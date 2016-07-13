f7020084f1a799064dcfb26832fababea1093526
Ian Dunn <ian@iandunn.name>
2016-07-13 12:42:41 -0700

    Move introduction message to `wp-config.php`.
    This needs to be in a very visible location, so that new developers taking over a project will see it and be able to learn how site is intended to be setup and maintained.

--- web/index.php
+++ web/index.php
@@ -1,12 +1,7 @@
 <?php
 
 /*
- * This installation is based on https://github.com/iandunn/regolith. See that URL for background information
- * and details.
- */
-
-/*
- * There has to be an `index.php` in the web root for this setup to work. So this is just a future-proof wrapper for
- * Core's `index.php`.
+ * There has to be an `index.php` in the web root for this folder structure to work. So this is just a
+ * future-proof wrapper for Core's `index.php`.
  */
 require_once( __DIR__ . '/wordpress/index.php' );
--- web/wp-config.php
+++ web/wp-config.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * This installation is based on https://github.com/iandunn/regolith. See that URL for documentation, etc.
+ */
+
 require_once( dirname( __DIR__ ) . '/config/environment.php'                          );
 require_once( dirname( __DIR__ ) . '/config/wordpress/common.php'                     );
 require_once( dirname( __DIR__ ) . '/config/wordpress/'. REGOLITH_ENVIRONMENT .'.php' );

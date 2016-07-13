06a7f7d43564eb5915540eab004314ef467aabfb
Ian Dunn <ian@iandunn.name>
2016-07-12 22:25:38 -0700

    Gitignore: Use absolute paths to avoid false positive matches.

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -7,8 +7,8 @@
 
 ROOT_PATH=$( dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) )
 ENVIRONMENT=$( grep 'REGOLITH_ENVIRONMENT' $ROOT_PATH/config/environment.php | awk -F "'" '{print $4}' )
-PLUGINS=$( grep 'content/plugins/' $ROOT_PATH/.gitignore |awk -F '/' '{print $4}' | tr '\n' ' ' )
-THEMES=$(  grep 'content/themes/'  $ROOT_PATH/.gitignore |awk -F '/' '{print $4}' | tr '\n' ' ' )
+PLUGINS=$( grep 'content/plugins/' $ROOT_PATH/.gitignore |awk -F '/' '{print $5}' | tr '\n' ' ' )
+THEMES=$(  grep 'content/themes/'  $ROOT_PATH/.gitignore |awk -F '/' '{print $5}' | tr '\n' ' ' )
 DEPLOYER_PATH="$ROOT_PATH/bin/deployer/deployer.phar"
 
 # Install Core

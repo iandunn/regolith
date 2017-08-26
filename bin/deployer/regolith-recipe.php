5d2ca70be34628cecd1d11451b5a38d5854591fe
Ian Dunn <ian@iandunn.name>
2017-08-26 15:46:47 -0700

    Deploy Recipe: Set default timezone to avoid PHP warning.

--- bin/deployer/regolith-recipe.php
+++ bin/deployer/regolith-recipe.php
@@ -3,6 +3,7 @@
 namespace Deployer;
 
 ini_set( 'display_errors', 1 );
+date_default_timezone_set( 'UTC' );
 
 require_once( 'recipe/common.php'                                            );
 require_once( dirname( dirname( __DIR__ ) ) . '/config/environment.php'      );
